76664bc70ebabce90d1faf9fb6d878e49a4a9e3d
bengiv <dev.bengiv@icloud.com>
2024-06-23 19:30:48 +0200

    wtf

--- src/main/java/dev/bengiv/EFDMTCWBTGPWB.java
+++ src/main/java/dev/bengiv/EFDMTCWBTGPWB.java
@@ -5,11 +5,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Dehydrationmodcookbottles implements ModInitializer {
+public class EFDMTCWBTGPWB implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("dehydration-mod-cook-bottles");
+    public static final Logger LOGGER = LoggerFactory.getLogger("EFDMTCWBTGPWB");
 
 	@Override
 	public void onInitialize() {
@@ -17,6 +17,6 @@ public class Dehydrationmodcookbottles implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("[EFDMTCWBTGPWB] init");
 	}
 }
--- src/main/java/dev/bengiv/FDMTCWBTGPWBDataGenerator.java
+++ src/main/java/dev/bengiv/FDMTCWBTGPWBDataGenerator.java
@@ -3,7 +3,7 @@ package dev.bengiv;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class DehydrationmodcookbottlesDataGenerator implements DataGeneratorEntrypoint {
+public class FDMTCWBTGPWBDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
